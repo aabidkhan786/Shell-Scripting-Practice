9f6eda06011fb51c45bcb213bd00ecaf5cd77f4e
Aabid <abidkhan786@outlook.com>
2022-04-04 16:35:53 +0530

    Factorial generator added

--- factorial.sh
+++ factorial.sh
@@ -9,3 +9,17 @@ do
     ((number--))
 done
 echo "Factorial of $num1 is: $fact"
+printf "\n\n"
+
+echo "Enter a number to find factorial upto"
+read fact_num
+num2=$fact_num
+for (( num=1; num<=fact_num; num++ ))
+do    
+fact1=1
+    for (( num1=1; num1<=num; num1++ ))
+    do
+        fact1=$(( num1*fact1 ))
+    done
+    echo "Factorial of $num is: $fact1"
+done

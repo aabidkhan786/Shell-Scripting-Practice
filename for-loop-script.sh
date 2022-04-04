ca77f68473f84872c21524eecaca73fae993b2b6
Aabid <abidkhan786@outlook.com>
2022-04-04 11:09:22 +0530

    Shell order reverse. Now it works.

--- for-loop-script.sh
+++ for-loop-script.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
-#The below code won't run out as the script runs in any shell (i.e sh).
-for (( count=0; count < 10; count++ ))
-do
-echo  -n "$count "
-done
-printf "\n"
-
 #!/bin/bash
 #The below code will run out as the script runs in bash shell (i.e bash).
 for (( counter=9; counter>=0; counter-- ))
 do
 echo -n "$counter "
+done
+printf "\n"
+
+#!/bin/sh
+#The below code won't run out as the script runs in any shell (i.e sh).
+for (( count=0; count < 10; count++ ))
+do
+echo  -n "$count "
 done
 printf "\n"
 

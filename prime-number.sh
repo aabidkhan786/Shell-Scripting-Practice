c7b79d9ac7195741ae41a21243a2463f84d0ae2b
Aabid <abidkhan786@outlook.com>
2022-04-04 13:06:29 +0530

    Prime Generator Issue Solved

--- prime-number.sh
+++ prime-number.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-echo "Type a number number to print out the prime number upto that: "
+echo "Type a number to print out the prime number upto that: "
 read prime_num
-for (( num=2; num <= prime_num; num++ ))
+for (( num=1; num <= prime_num; num++ ))
 do
     prime=0
     for (( checkNum=2; checkNum <= num; checkNum++ ))
     do
-        if [[ $num % $checkNum == 0 ]]
+        if (( $num%$checkNum == 0 ))
         then
-            prime++
-            echo "$prime"
+            ((prime++))
         fi
     done
     if [[ $prime < 2 ]]
@@ -19,4 +18,4 @@ do
 done
 printf "\n"
 
-#There's an error in the programm, as the prime variable is not incremented. So for that reason it print's out all the number.
+#Successfully Run Out

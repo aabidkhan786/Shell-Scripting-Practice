08750cd60d4b8d502d8da8e3aa9130c159051499
aabidkhan786 <abidkhan786@outlook.com>
2022-04-05 11:04:16 +0530

    Directly reading the ouput through curl

--- get_weather.sh
+++ get_weather.sh
@@ -3,10 +3,9 @@ if [[ $1 == '' ]]
 then
     echo "You have give city name as argument like: bash get_weather.json city-name"
 else
-    curl -s https://wttr.in/$1?format=j1 jq . > weather.json
-    getdate=$(jq .current_condition[0].localObsDateTime weather.json)
-    getweather=$(jq .current_condition[0].weatherDesc[0].value weather.json)
-    getC=$(jq .current_condition[0].FeelsLikeC weather.json)
-    getF=$(jq .current_condition[0].FeelsLikeF weather.json)
+    getdate=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].localObsDateTime')
+    getweather=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].weatherDesc[0].value')
+    getC=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].FeelsLikeC')
+    getF=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].FeelsLikeF')
     echo "On $getdate $1 is set to be $getweather with the temperature feels like $getC C | $getF F"
 fi

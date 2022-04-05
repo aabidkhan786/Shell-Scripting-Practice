98801e84453128ffe3fe5a23045e50ed3ec3fe9c
aabidkhan786 <abidkhan786@outlook.com>
2022-04-05 10:44:27 +0530

    Dynamic City Weather Added

--- get_weather.sh
+++ get_weather.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-getdate=$(jq .current_condition[0].localObsDateTime weather.json)
-getweather=$(jq .current_condition[0].weatherDesc[0].value weather.json)
-getC=$(jq .current_condition[0].FeelsLikeC weather.json)
-getF=$(jq .current_condition[0].FeelsLikeF weather.json)
-echo "On $getdate London is set to be $getweather with the temperature feels like $getC C | $getF F"
+if [[ $1 == '' ]]
+then
+    echo "You have give city name as argument like: bash get_weather.json city-name"
+else
+    curl -s https://wttr.in/$1?format=j1 jq . > weather.json
+    getdate=$(jq .current_condition[0].localObsDateTime weather.json)
+    getweather=$(jq .current_condition[0].weatherDesc[0].value weather.json)
+    getC=$(jq .current_condition[0].FeelsLikeC weather.json)
+    getF=$(jq .current_condition[0].FeelsLikeF weather.json)
+    echo "On $getdate $1 is set to be $getweather with the temperature feels like $getC C | $getF F"
+fi

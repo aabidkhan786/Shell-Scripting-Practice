75789bb60ae63f24cdc063bd4aa222a78c060310
aabidkhan786 <abidkhan786@outlook.com>
2022-04-05 16:06:24 +0530

    Single Curl Usage

--- get_weather.sh
+++ get_weather.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [[ $1 == '' ]]
 then
-    echo "You have give city name as argument like: bash get_weather.json city-name"
+    echo "You have to give city name as argument like: bash get_weather.sh city-name"
 else
-    getdate=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].localObsDateTime')
-    getweather=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].weatherDesc[0].value')
-    getC=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].FeelsLikeC')
-    getF=$(curl -s https://wttr.in/$1?format=j1 | jq '.current_condition[0].FeelsLikeF')
-    echo "On $getdate $1 is set to be $getweather with the temperature feels like $getC C | $getF F"
+    weatherData=$(curl -s https://wttr.in/$1?format=j1)
+    date=$(echo $weatherData | jq '.current_condition[0].localObsDateTime')
+    cel=$(echo $weatherData | jq '.current_condition[0].FeelsLikeC')
+    far=$(echo $weatherData | jq '.current_condition[0].FeelsLikeF')
+    desc=$(echo $weatherData | jq '.current_condition[0].weatherDesc[0].value')
+    echo "On $date $1 is set to be $desc with the temperature feels like $cel C | $far F"
 fi
